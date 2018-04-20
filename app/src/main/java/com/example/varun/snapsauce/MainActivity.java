f2105b2c210513d2fc50308273206abcb51f735e
Varun <varun.khatri@sjsu.edu>
2018-04-20 14:45:02 -0700

    Initial activity

---
+++ app/src/main/java/com/example/varun/snapsauce/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.varun.snapsauce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
